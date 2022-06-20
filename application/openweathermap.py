c67069b307d026e364dc0301187b048299c00b1a
L1geon <ligeon1@piterov.ru>
2022-06-20 17:47:48 +0300

    first commit

---
+++ application/openweathermap.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def get_weather(city: str):
+    api = "c5c26494dfd18dbbd2763e83817527f8"
+    params = {"q": city, "type": "like", "units": "metric", "lang": "ru", "APPID": api}
+    r = requests.get("http://api.openweathermap.org/data/2.5/find", params=params)
+    data = r.json()
+    print(f"""        id города в системе OpenWeatherMap: {data["list"][0]["id"]}
+        Координаты города: {data["list"][0]["coord"]}
+        Текущая температура: {data["list"][0]["main"]["temp"]}°C
+        Облака: {data["list"][0]["weather"][0]["description"]}""")
+
+if __name__ == "__main__":
+    get_weather()
---
+++ main.py
@@ -0,0 +1,13 @@
+from application.openweathermap import *
+
+def main():
+    city = input("Введите город (на английском):")
+    get_weather(city)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
