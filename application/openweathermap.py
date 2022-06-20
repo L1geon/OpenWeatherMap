1d3fae911f04f040742029c676fa343b72f4c93d
L1geon <ligeon1@piterov.ru>
2022-06-20 17:51:05 +0300

    deleted api

--- application/openweathermap.py
+++ application/openweathermap.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_weather(city: str):
-    api = "c5c26494dfd18dbbd2763e83817527f8"
+    api = ""
     params = {"q": city, "type": "like", "units": "metric", "lang": "ru", "APPID": api}
     r = requests.get("http://api.openweathermap.org/data/2.5/find", params=params)
     data = r.json()
@@ -10,6 +10,7 @@ def get_weather(city: str):
         Координаты города: {data["list"][0]["coord"]}
         Текущая температура: {data["list"][0]["main"]["temp"]}°C
         Облака: {data["list"][0]["weather"][0]["description"]}""")
+
 
 if __name__ == "__main__":
     get_weather()
